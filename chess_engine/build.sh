5378f969096e7a28f06cf1b938ed5fe115ba49ae
kabixtha <kabis4039@gmail.com>
2026-04-27 17:11:12 -0400

    Fix build script for Render

--- chess_engine/build.sh
+++ chess_engine/build.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
-set -e
+set -o errexit
 
-echo "Installing system packages..."
-apt-get update -qq
-apt-get install -y stockfish
-
-echo "Installing Python dependencies..."
 pip install -r requirements.txt
-
-echo "Collecting static files..."
 python manage.py collectstatic --no-input
-
-echo "Build complete!"
+python manage.py migrate
