495404ec3d308b8f8620b62df4ccf011e38f3749
kabixtha <kabis4039@gmail.com>
2026-04-27 16:05:31 -0400

    Fix build.sh path

---
+++ chess_engine/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+cd chess_engine
+pip install -r requirements.txt
+wget -q "https://github.com/official-stockfish/Stockfish/releases/download/sf_17/stockfish-ubuntu-x86-64-avx2.tar"
+tar -xf stockfish-ubuntu-x86-64-avx2.tar
+cp stockfish/stockfish-ubuntu-x86-64-avx2 /opt/render/project/src/stockfish_bin
+chmod +x /opt/render/project/src/stockfish_bin
+python manage.py collectstatic --noinput
