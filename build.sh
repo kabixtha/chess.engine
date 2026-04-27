202cbf9cc75f1819e539bb8c552ac58ea4bd7374
kabixtha <kabis4039@gmail.com>
2026-04-27 16:07:40 -0400

    Keep correct build.sh only

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-pip install -r requirements.txt
-wget -q "https://github.com/official-stockfish/Stockfish/releases/download/sf_17/stockfish-ubuntu-x86-64-avx2.tar"
-tar -xf stockfish-ubuntu-x86-64-avx2.tar
-cp stockfish/stockfish-ubuntu-x86-64-avx2 /opt/render/project/src/stockfish_bin
-chmod +x /opt/render/project/src/stockfish_bin
-python manage.py collectstatic --noinput
